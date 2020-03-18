16fe2d47b1e50bff360173c99cdc34bb5c8346c2
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-03-18 16:56:29 -0400

    Adding in pwquality file and setting options for pam.d

--- manifests/auth/pam.pp
+++ manifests/auth/pam.pp
@@ -7,7 +7,7 @@
 #
 class cis_hardening::auth::pam {
 
-  # Ensure Password creation requirements are configured
+  # Ensure Password creation requirements are configured - Section 5.3.1
   file { '/etc/security/pwquality.conf':
     ensure => 'present',
     owner  => 'root',
@@ -15,4 +15,30 @@ class cis_hardening::auth::pam {
     mode   => '0644',
     source => 'puppet:///modules/cis_hardening/pwquality.conf',
   }
+
+  # Ensure lockout delay for failed password attempts is configured - Section 5.3.2
+    exec {'enable_lockout_delay_sysauthac':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+      command => "perl -i -e 's/pam_faildelay.so.*$/pam_faildelay.so delay=2000000/' /etc/pam.d/system-auth-ac",
+      unless  => "grep 'delay=2000000' /etc/pam.d/system-auth-ac",
+    }
+
+    exec { 'enable_lockout_delay_passauthac':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+      command => "perl -i -e 's/pam_faildelay.so.*$/pam_faildelay.so delay=2000000/' /etc/pam.d/password-auth-ac",
+      unless  => "grep 'delay=2000000' /etc/pam.d/password-auth-ac",
+    }
+
+  # Ensure Password Reuse is Limited - Section 5.3.3
+    exec { 'limit_pw_reuse_sysauthac':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+    command => "perl -pi -e's/$/ remember=5/ if /^password\s+sufficient\s+pam_unix.so/' /etc/pam.d/system-auth-ac",
+    unless  => "grep 'remember=5' /etc/pam.d/system-auth-ac",
+    }
+
+    exec { 'limit_pw_reuse_passauthac':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+      command => "perl -pi -e's/$/ remember=5/ if /^password\s+sufficient\s+pam_unix.so/' /etc/pam.d/password-auth-ac",
+      unless  => "grep 'remember=5' /etc/pam.d/password-auth-ac",
+    }
 }
