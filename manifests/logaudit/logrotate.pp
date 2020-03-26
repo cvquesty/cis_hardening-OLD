99943ce4d885e9a3117e7a23b9d56ed38f44383a
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-03-26 10:27:28 -0400

    Added logging requirements and all section 4.x needs for CIS hardneing.

--- manifests/logaudit/logrotate.pp
+++ manifests/logaudit/logrotate.pp
@@ -1,8 +1,14 @@
-# @summary A short summary of the purpose of this class
+# @summary A manifest to configure LogRotate on the server
 #
-# A description of what this class does
+# Section 4.3
 #
 # @example
 #   include cis_hardening::logaudit::logrotate
 class cis_hardening::logaudit::logrotate {
+
+  # LogRotate will be different per site and the needs of the security department will
+  # dictate what and how these are to be configured. I recommend using the puppet-logrotate
+  # module from the Puppet Forge, and instrumenting it with Hiera + a profile to be applied
+  # to systems that need their logs rotated.
+
 }
