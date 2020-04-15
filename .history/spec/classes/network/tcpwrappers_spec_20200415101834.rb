66bdfe193695895d85cdbfa721de4682995cdd69
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-04-15 10:22:14 -0400

    Completed tests for the tcpwrappers manifest.

--- .history/spec/classes/network/tcpwrappers_spec_20200415101834.rb
+++ .history/spec/classes/network/tcpwrappers_spec_20200415101834.rb
@@ -13,14 +13,6 @@ describe 'cis_hardening::network::tcpwrappers' do
         'ensure' => 'present',
       )}
 
-      # Ensure /etc/hosts.allow is configured - Section 3.4.2, 3.4.4
-      it { is_expected.to contain_file('/etc/hosts.allow').with(
-        'ensure' => 'present',
-        'owner'  => 'root',
-        'group'  => 'root',
-        'mode'   => '0644',
-      )}
-
       # Ensure it compiles with all dependencies
       it { is_expected.to compile.with_all_deps }
     end
