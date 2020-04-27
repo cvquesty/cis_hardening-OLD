316bff176369644572c0a987ef282e318d842459
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-04-27 18:03:03 -0400

    Commits for aide manifest to facilitate fim

--- .history/spec/classes/setup/fim_spec_20200427175450.rb
+++ .history/spec/classes/setup/fim_spec_20200427175450.rb
@@ -27,12 +27,7 @@ describe 'cis_hardening::setup::fim' do
         'unless'  => 'test -f /var/lib/aide/aide.db.gz',        
       ).that_requires('Exec[aideinit]')}
 
-      it { is_expected.to contain_cron('aide').with(
-        'command' => '/usr/sbin/aide --check',
-        'user'    => 'root',
-        'hour'    => 5,
-        'minute'  => 0,
-      )}
+      
 
       # Ensure the manifest compiles with all dependencies
       it { is_expected.to compile.with_all_deps }
