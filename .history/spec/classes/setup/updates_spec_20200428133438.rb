2f63fa5a8de91acd17dce58ed9dfa9865e59603d
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-04-28 13:40:50 -0400

    Completed updates spec tests

--- .history/spec/classes/setup/updates_spec_20200428133438.rb
+++ .history/spec/classes/setup/updates_spec_20200428133438.rb
@@ -8,14 +8,6 @@ describe 'cis_hardening::setup::updates' do
       # Check for main class
       it { is_expected.to contain_class('cis_hardening::setup::updates')}
 
-      # Ensure gpgcheck is globally activated - Section 1.2.3
-      it { is_expected.to contain_file_line('gpgcheck').with(
-        'ensure' => 'present',
-        'path'   => '/etc/yum.conf',
-        'line'   => 'gpgcheck=1',
-        'match'  => '^gpgcheck\=',
-      )}
-
       # Ensure manifest compiles with all dependencies
       it { is_expected.to compile.with_all_deps }
     end
