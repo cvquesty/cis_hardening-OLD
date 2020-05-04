28c774b2ff3a58f8ee68bc43983bcc37898386cd
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-05-04 13:35:17 -0400

    Completed all examples for codebase

--- .history/spec/classes/services/svcclients_spec_20200504132947.rb
+++ .history/spec/classes/services/svcclients_spec_20200504132947.rb
@@ -33,6 +33,7 @@ describe 'cis_hardening::services::svcclients' do
         'ensure' => 'absent',
       )}
 
+
       # Ensure manifest compiles with all dependencies
       it { is_expected.to compile.with_all_deps }
     end
