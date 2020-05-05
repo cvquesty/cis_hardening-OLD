5b5dd435d4de06d99993e4f93338dc1971a6d3e0
Jerald Sheets <jsheets@ssconsultinggroup.net>
2020-05-05 16:14:11 -0400

    Resolved Rubocop issues and re-installed pdk to make sure everything current and accurate

--- .history/manifests/init_20200226135239.pp
+++
@@ -1,5 +0,0 @@
-# @summary A module to implement CIS hardening standards for CentOS 7.x Systems
-#
-#
-class cis_hardening {
-}
--- spec/classes/setup_spec.rb
+++ spec/classes/setup_spec.rb
@@ -6,16 +6,16 @@ describe 'cis_hardening::setup' do
       let(:facts) { os_facts }
 
       # Check for default class
-      it { is_expected.to contain_class('cis_hardening::setup')}
+      it { is_expected.to contain_class('cis_hardening::setup') }
 
       # Check for all includes in the setup.pp
-      it { is_expected.to contain_class('cis_hardening::setup::updates')}
-      it { is_expected.to contain_class('cis_hardening::setup::secboot')}
-      it { is_expected.to contain_class('cis_hardening::setup::prochardening')}
-      it { is_expected.to contain_class('cis_hardening::setup::fim')}
-      it { is_expected.to contain_class('cis_hardening::setup::filesystem')}
-      it { is_expected.to contain_class('cis_hardening::setup::banners')}
-      it { is_expected.to contain_class('cis_hardening::setup::accessctl')}
+      it { is_expected.to contain_class('cis_hardening::setup::updates') }
+      it { is_expected.to contain_class('cis_hardening::setup::secboot') }
+      it { is_expected.to contain_class('cis_hardening::setup::prochardening') }
+      it { is_expected.to contain_class('cis_hardening::setup::fim') }
+      it { is_expected.to contain_class('cis_hardening::setup::filesystem') }
+      it { is_expected.to contain_class('cis_hardening::setup::banners') }
+      it { is_expected.to contain_class('cis_hardening::setup::accessctl') }
 
       # Ensure manifest compiles with all dependencies
       it { is_expected.to compile.with_all_deps }
